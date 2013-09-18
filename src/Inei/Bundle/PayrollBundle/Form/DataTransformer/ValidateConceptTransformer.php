c0ea2d6d04a3063773f91f4dc7b03ab60e3f9a51
heraldmatias <heraldmatias.oz@gmail.com>
2013-09-18 18:16:40 -0500

    save and planillas

--- src/Inei/Bundle/PayrollBundle/Form/DataTransformer/ValidateConceptTransformer.php
+++ src/Inei/Bundle/PayrollBundle/Form/DataTransformer/ValidateConceptTransformer.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * Description of ValidateConceptTransformer
  *
  * @author holivares
  */
+
 namespace Inei\Bundle\PayrollBundle\Form\DataTransformer;
 
 use Symfony\Component\Form\DataTransformerInterface;
@@ -11,8 +13,8 @@ use Symfony\Component\Form\Exception\TransformationFailedException;
 use Doctrine\Common\Persistence\ObjectManager;
 use Inei\Bundle\PayrollBundle\Entity\Conceptos;
 
-class ValidateConceptTransformer implements DataTransformerInterface
-{
+class ValidateConceptTransformer implements DataTransformerInterface {
+
     /**
      * @var ObjectManager
      */
@@ -21,8 +23,7 @@ class ValidateConceptTransformer implements DataTransformerInterface
     /**
      * @param ObjectManager $om
      */
-    public function __construct(ObjectManager $om)
-    {
+    public function __construct(ObjectManager $om) {
         $this->om = $om;
     }
 
@@ -32,14 +33,13 @@ class ValidateConceptTransformer implements DataTransformerInterface
      * @param  Conceptos|null $concept
      * @return string
      */
-    public function transform($concepts)
-    {
+    public function transform($concepts) {
         if (null === $concepts) {
             return null;
         }
-        /*if ($concept instanceof \Doctrine\ORM\PersistentCollection){
-            return null;
-        }*/
+        /* if ($concept instanceof \Doctrine\ORM\PersistentCollection){
+          return null;
+          } */
 //        $p = new \Doctrine\ORM\PersistentCollection();
 //        $p->set($key, $value);
 //        $p->removeElement($element);
@@ -55,44 +55,39 @@ class ValidateConceptTransformer implements DataTransformerInterface
      *
      * @throws TransformationFailedException if object (issue) is not found.
      */
-    public function reverseTransform($concepts)
-    {
+    public function reverseTransform($concepts) {
         if (!$concepts) {
             return null;
         }
         $toremove = array();
         $topersist = array();
-        /*SI ES UNA NUEVA COLLECCION DE CONCEPTOS*/
-        if(!$concepts instanceof \Doctrine\ORM\PersistentCollection){
+        /* SI ES UNA NUEVA COLLECCION DE CONCEPTOS */
+        if (!$concepts instanceof \Doctrine\ORM\PersistentCollection) {
             foreach ($concepts as $key => $concept) {
                 $_concept = $this->om
-                ->getRepository('IneiPayrollBundle:Conceptos')
-                ->findOneBy(array('codiConcTco' => $concept->getCodiConcTco()));
-                if($_concept){
+                        ->getRepository('IneiPayrollBundle:Conceptos')
+                        ->findOneBy(array('codiConcTco' => $concept->getCodiConcTco()));
+                if ($_concept) {
                     $topersist[] = $_concept;
                 }
             }
             return $topersist;
         }
-        /*SI ES QUE ES UNA ACTUALIZACION DE LOS CONCEPTOS*/
+        /* SI ES QUE ES UNA ACTUALIZACION DE LOS CONCEPTOS */
         foreach ($concepts as $key => $concept) {
             $_concept = $this->om
-            ->getRepository('IneiPayrollBundle:Conceptos')
-            ->findOneBy(array('codiConcTco' => $concept->getCodiConcTco()));
-            if(!$_concept){
+                    ->getRepository('IneiPayrollBundle:Conceptos')
+                    ->findOneBy(array('codiConcTco' => $concept->getCodiConcTco()));
+            if (!$_concept) {
                 $toremove[] = $concept;
-            }else{
+            } else {
                 $topersist[] = $_concept;
             }
-        }        
-//        foreach ($toremove as $key => $value) {            
-//            $concepts->removeElement($value);            
-//        }
-        $concepts->clear();
-        foreach ($topersist as $key => $value) {         
+        }
+        foreach ($topersist as $key => $value) {
             $concepts->set($key, $value);
-        }        
+        }
         return $concepts;
-
     }
+
 }
