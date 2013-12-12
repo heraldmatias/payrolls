0d13f533f1868a031f0ed07dfc06608ebe46ac9e
heraldmatias <heraldmatias.oz@gmail.com>
2013-12-12 11:31:12 -0500

    delete tomo

--- src/Inei/Bundle/PayrollBundle/Service/TomosService.php
+++ src/Inei/Bundle/PayrollBundle/Service/TomosService.php
@@ -31,4 +31,22 @@ class TomosService {
         return $this->em->getRepository('IneiPayrollBundle:Tomos')
                 ->findTomo(is_numeric($tomo)?$tomo:0);
     }
+    
+    public function deleteTomo($tomo){
+        $con = $this->em->getConnection();
+        /**/        
+        $del1 = "DELETE FROM planilla_historicas WHERE codi_folio IN 
+            (SELECT codi_folio FROM folios WHERE codi_tomo = $tomo)";
+        $del2 = "DELETE FROM conceptos_folios WHERE codi_folio IN 
+            (SELECT codi_folio FROM folios WHERE codi_tomo = $tomo)";
+        $del3 = "DELETE FROM folios WHERE codi_tomo = $tomo";
+        $del4 = "DELETE FROM asignacion where co_tomo= $tomo";
+        $del5 = "DELETE FROM tomos WHERE codi_tomo = $tomo";
+        $con->executeUpdate($del1);
+        $con->executeUpdate($del2);
+        $con->executeUpdate($del3);
+        $con->executeUpdate($del4);
+        $con->executeUpdate($del5);
+    }
+    
 }
