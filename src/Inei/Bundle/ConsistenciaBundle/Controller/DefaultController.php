a08f07da190db6fc4419b85dfbf1393a9e96da09
heraldmatias <heraldmatias.oz@gmail.com>
2014-01-09 12:49:26 -0500

    buscar personal autocomplete

--- src/Inei/Bundle/ConsistenciaBundle/Controller/DefaultController.php
+++ src/Inei/Bundle/ConsistenciaBundle/Controller/DefaultController.php
@@ -7,19 +7,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use \PHPExcel_IOFactory;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * 
      * @param type $name
      * @Route("/consistencia/", name="_consistencia_index")
      * @Template("")
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $service = $this->get('consistencia_service');
-        
+
 //        $query = $service->findPersonal();
 //        $paginator = $this->get('knp_paginator');
 //        $pagination = $paginator->paginate(
@@ -31,34 +31,120 @@ class DefaultController extends Controller
             'sincroniza' => $sincroniza
         );
     }
-    
+
     /**
      * 
      * @param type $name
      * @Route("/consistencia/proceso/", name="_consistencia_procesar")
      * @Template("")
      */
-    public function consistenciaAction()
-    {
-        $service = $this->get('consistencia_service');
+    public function consistenciaAction() {
+//        $service = $this->get('consistencia_service');
 //        $query = $service->findPersonal();
 //        $paginator = $this->get('knp_paginator');
 //        $pagination = $paginator->paginate(
 //                $query, $this->get('request')->query->get('page', 1)/* page number */, 15/* limit per page */
 //        );        
         return array(
-            #'pagination' => $pagination            
+                #'pagination' => $pagination            
         );
     }
-    
+
+    /**
+     * 
+     * @param type $name
+     * @Route("/consistencia/proceso/periodo/", name="_consistencia_periodo")
+     * @Template("")
+     */
+    public function consistenciaPeriodoAction() {
+        $file = null;
+        $target_path = __DIR__ . '/../../../../../web/upload/consistencia/';
+        if (isset($_FILES['periodo'])) {
+            if (!is_dir($target_path)) {
+                //echo $this->getUploadRootDir();
+                mkdir($target_path);
+                chmod($target_path, 0755);
+            }
+            $fileName = basename($_FILES['periodo']['name']);
+            $target_path .= $fileName;
+            if (move_uploaded_file($_FILES['periodo']['tmp_name'], $target_path)) {
+//                chmod($target_path .= $fileName, 0644);
+            }
+            $file = $fileName;
+        } else {
+            $gestor = @opendir($target_path);
+            if ($gestor) {
+                while (false !== ($entrada = readdir($gestor))) {
+                    if ($entrada !== '.' & $entrada !== null)
+                        $file = $entrada;
+                    //break;
+                }
+                closedir($gestor);
+            }
+        }
+        return array(
+            'file' => $file
+        );
+    }
+
+    /**
+     * 
+     * @param type $name
+     * @Route("/consistencia/proceso/periodo/ajax/", name="_consistencia_periodo_ajax")
+     * @Template("")
+     */
+    public function consistenciaPeriodoAjaxAction() {
+        $data = array(
+            'error' => null,
+            'success' => true,
+            'data' => 'Ok'
+        );
+        try {
+            $file = null;
+            $target_path = __DIR__ . '/../../../../../web/upload/consistencia/';
+            $gestor = @opendir($target_path);
+            if ($gestor) {
+                while (false !== ($entrada = readdir($gestor))) {
+                    if ($entrada !== '.' & $entrada !== null)
+                        $file = $target_path. $entrada;
+                }
+                closedir($gestor);
+            }
+            if ($file) {
+                $objPHPExcel = PHPExcel_IOFactory::load($file);
+                $sheet = $objPHPExcel->getSheet(0);
+                $service = $this->get('consistencia_service');
+                $periodo = null;
+                $fila = 1;
+                while (true) {
+                    $periodo = $sheet->getCellByColumnAndRow(0, $fila)->getValue();
+                    $nperiodo = $sheet->getCellByColumnAndRow(1, $fila)->getValue();
+                    
+                    if ($periodo === '' | $periodo === null) {
+                        break;
+                    }
+                    if ($nperiodo !== '' | $nperiodo !== null) {
+                        $service->actualizaPeriodo($nperiodo, $periodo);
+                    }
+                    $fila++;
+                }
+            }
+        } catch (Exception $e) {
+            $data['error'] = $e->getMessage();
+            $data['success'] = false;
+        }
+        $response = new Response(json_encode($data));
+        $response->headers->set('content-type', 'application/json');
+        return $response;
+    }
+
     /**
      * 
      * @param type $name
      * @Route("/consistencia/proceso/manual/", name="_consistencia_manual_procesar")
      * @Template("")
      */
-    public function consistenciaManualAction(Request $request)
-    {
+    public function consistenciaManualAction(Request $request) {
         $service = $this->get('consistencia_service');
         $criteria = array();
         $tipo = 1;
@@ -67,25 +153,28 @@ class DefaultController extends Controller
             'nombres' => '',
             'codigo' => ''
         );
-        if($request->query->has('form')){
-            $criteria = $request->query->get('form');            
+        if ($request->query->has('form')) {
+            $criteria = $request->query->get('form');
             foreach ($criteria as $key => $value) {
                 if (!$value) {
                     unset($criteria[$key]);
                 }
             }
             $form = $request->query->get('form');
-        }        
-        if($request->query->has('tipo_busqueda')){
+        }
+        if ($request->query->has('tipo_busqueda')) {
             $tipo = $request->query->get('tipo_busqueda');
         }
-        switch ($tipo){
+        switch ($tipo) {
             case 1:
-                $query = $service->findPersonalNoEncontrado($criteria);break;
+                $query = $service->findPersonalNoEncontrado($criteria);
+                break;
             case 2:
-                $query = $service->findPersonalEncontrado($criteria);break;
+                $query = $service->findPersonalEncontrado($criteria);
+                break;
             case 3:
-                $query = $service->findPersonal($criteria);break;
+                $query = $service->findPersonal($criteria);
+                break;
             default :
                 $query = $service->findPersonalNoEncontrado($criteria);
         }
@@ -94,58 +183,57 @@ class DefaultController extends Controller
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
                 $query, $this->get('request')->query->get('page', 1)/* page number */, 25/* limit per page */
-        );        
+        );
         return array(
             'pagination' => $pagination,
             'form' => $form
-            #'familias' => $familias
+                #'familias' => $familias
         );
     }
-    
+
     /**
      * @Route("/consistencia/ajax/", name="_consistencia_personal_ajax")     
      */
-    public function iniciaConsistenciaAction(){
+    public function iniciaConsistenciaAction() {
         $service = $this->get('consistencia_service');
-        
+
         $result = $service->getPersonalDigitado();
         $data = array(
             'success' => $result !== -1,
             'data' => $result,
-            'error' => $result === -1?
-                'Ocurrio un error al consistenciar los empleados':''
+            'error' => $result === -1 ?
+                    'Ocurrio un error al consistenciar los empleados' : ''
         );
         $response = new Response(json_encode($data));
         $response->headers->set('Content-type', 'application/json');
         return $response;
     }
-    
+
     /**
      * @Route("/consistencia/proceso/ajax/", name="_consistencia_proceso_ajax")     
      */
-    public function procesaConsistenciaAction(){
+    public function procesaConsistenciaAction() {
         $service = $this->get('consistencia_service');
-        
+
         $result = $service->processPersonalTodo();
         $data = array(
             'success' => $result !== -1,
             'data' => $result,
-            'error' => $result === -1?
-                'Ocurrio un error al procesar los empleados':''
+            'error' => $result === -1 ?
+                    'Ocurrio un error al procesar los empleados' : ''
         );
         $response = new Response(json_encode($data));
         $response->headers->set('Content-type', 'application/json');
         return $response;
     }
-    
-    
+
     /**
      * @Route("/consistencia/personal/ajax/", name="_maestro_personal_ajax")     
      */
-    public function getPersonalSigaAction(Request $request){
+    public function getPersonalSigaAction(Request $request) {
         $service = $this->get('consistencia_service');
-        $nombres = array();
-        if($request->query->has('personal')){
+        $nombres = '';
+        if ($request->query->has('personal')) {
             $nombres = $request->query->get('personal');
         }
         $result = $service->findPersonalSiga($nombres);
@@ -158,18 +246,18 @@ class DefaultController extends Controller
         $response->headers->set('Content-type', 'application/json');
         return $response;
     }
-    
+
     /**
      * @Route("/consistencia/personal-asocia/ajax/", name="_personal_asocia_ajax")
      */
-    public function personalAsociaAction(Request $request){
+    public function personalAsociaAction(Request $request) {
         $service = $this->get('consistencia_service');
         $nombres = array();
         $persona = NULL;
-        if($request->query->has('personal')){
+        if ($request->query->has('personal')) {
             $nombres = $request->query->get('personal');
         }
-        if($request->query->has('persona')){
+        if ($request->query->has('persona')) {
             $persona = $request->query->get('persona');
         }
         $result = $service->asociarPersonal($nombres, $persona);
@@ -182,35 +270,35 @@ class DefaultController extends Controller
         $response->headers->set('Content-type', 'application/json');
         return $response;
     }
-    
+
     /**
      * @Route("/consistencia/personal-registra/ajax/", name="_personal_registra_ajax")
      */
-    public function registraPersonalAction(Request $request){
+    public function registraPersonalAction(Request $request) {
         $service = $this->get('consistencia_service');
         $result = false;
-        if($request->query->has('form')){
+        if ($request->query->has('form')) {
             $form = $request->query->get('form');
             $result = $service->registraPersona($form);
         }
         $data = array(
             'success' => $result,
             'data' => $result,
-            'error' => $result?'':'Ocurrio un error al registrar la persona'
+            'error' => $result ? '' : 'Ocurrio un error al registrar la persona'
         );
         $response = new Response(json_encode($data));
         $response->headers->set('Content-type', 'application/json');
         return $response;
     }
-    
+
     /**
      * @Route("/consistencia/personal/print/", name="_personal_print")
      */
-    public function imprimir(Request $request){
+    public function imprimir(Request $request) {
         $service = $this->get('consistencia_service');
         $criteria = array();
-        if($request->query->has('form')){
-            $criteria = $request->query->get('form');            
+        if ($request->query->has('form')) {
+            $criteria = $request->query->get('form');
             foreach ($criteria as $key => $value) {
                 if (!$value) {
                     unset($criteria[$key]);
@@ -218,20 +306,23 @@ class DefaultController extends Controller
             }
         }
         $tipo = 1;
-        if($request->query->has('tipo_busqueda')){
+        if ($request->query->has('tipo_busqueda')) {
             $tipo = $request->query->get('tipo_busqueda');
         }
-        switch ($tipo){
+        switch ($tipo) {
             case 1:
-                $query = $service->findPersonalNoEncontrado($criteria);break;
+                $query = $service->findPersonalNoEncontrado($criteria);
+                break;
             case 2:
-                $query = $service->findPersonalEncontrado($criteria);break;
+                $query = $service->findPersonalEncontrado($criteria);
+                break;
             case 3:
-                $query = $service->findPersonal($criteria);break;
+                $query = $service->findPersonal($criteria);
+                break;
             default :
                 $query = $service->findPersonalNoEncontrado($criteria);
         }
-        $data = $query->getArrayResult();//print_r($data);exit;
+        $data = $query->getArrayResult(); //print_r($data);exit;
         $response = new Response();
         $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         $response->headers->set('Content-Disposition', sprintf('attachment;filename="%s.csv"', 'personal'));
@@ -240,4 +331,5 @@ class DefaultController extends Controller
         echo $service->printCSV($data);
         exit;
     }
+
 }
