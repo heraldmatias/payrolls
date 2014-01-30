92d60278e5dfe65dee82870a72040f14b92b77ff
heraldmatias <heraldmatias.oz@gmail.com>
2014-01-30 17:46:19 -0500

    actualizacion masiva de inventario de tomo

--- src/Inei/Bundle/PayrollBundle/Repository/FoliosRepository.php
+++ src/Inei/Bundle/PayrollBundle/Repository/FoliosRepository.php
@@ -92,6 +92,57 @@ class FoliosRepository extends EntityRepository {
         $folio->setConceptos($qbc->getResult());
         return $folio;
     }
+    
+    public function findCustomByNum($folio, $tomo) {
+        $DQL = "SELECT partial f.{codiFolio}
+            FROM IneiPayrollBundle:Folios f
+            WHERE f.folio = :folio AND f.tomo = :tomo";
+        $qb = $this->getEntityManager()->createQuery($DQL)
+                ->setParameters(array(
+            'folio' => is_numeric($folio)?$folio:0,
+            'tomo' => is_numeric($tomo)?$tomo:0
+        ));
+        $re = $qb->getArrayResult();
+        if(!$re)
+            return null;
+        $folio = $re[0]['codiFolio'];
+        /*         * *CONCEPTOS*** */
+        $DQL = "SELECT partial cf.{id}, partial c.{codiConcTco}
+            FROM IneiPayrollBundle:ConceptosFolios cf
+            JOIN cf.codiConcTco c
+            WHERE cf.codiFolio = :pk ORDER BY cf.orden";
+        $qbc = $this->getEntityManager()->createQuery($DQL)
+                ->setParameters(array(
+            'pk' => $folio,
+        ));        
+        $lista = $qbc->getArrayResult();
+        $conceptos['folio'] = $folio;
+        $conceptos['conceptos'] = array_map(create_function('$item', 'return array($item["codiConcTco"]["codiConcTco"], $item["id"]);'), $lista);
+        return $conceptos;
+    }
+    
+    public function findFolioInventarioByNum($folio, $tomo) {
+        /*folios*/
+        $sql = "SELECT codi_folio, num_folio, per_folio, reg_folio, tipo_plan_tpl FROM folios WHERE codi_tomo = :tomo AND num_folio=:folio LIMIT 1;";
+        $st = $this->getEntityManager()->getConnection()->prepare($sql);
+        $st->bindValue(1, $tomo);
+        $st->bindValue(2, $folio);
+        $st->execute();
+        $folio = $st->fetchAll(\Doctrine\ORM\Query::HYDRATE_SCALAR);
+        $data = null;
+        /*conceptos*/
+        if($folio){
+            $sql = "SELECT codi_conc_tco FROM conceptos_folios WHERE codi_folio = :folio ORDER BY orden_conc_folio ASC;";
+            $st2 = $this->getEntityManager()->getConnection()->prepare($sql);
+            $st2->bindValue(1, $folio[0][0]);
+            $st2->execute();
+            unset($folio[0][0]);
+            $_conceptos = $st2->fetchAll(\Doctrine\ORM\Query::HYDRATE_SCALAR);
+            $conceptos = array_map(create_function('$item', 'return $item[0];'), $_conceptos);
+            $data = array_merge($folio[0], $conceptos);
+        }
+        return $data;
+    }
 
     public function findResumenFolios(array $filtro = null) {
         $where1 = array();
