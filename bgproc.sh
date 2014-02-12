689829f2583c00525d9e049a3ec987805b0189ac
heraldmatias <heraldmatias.oz@gmail.com>
2014-02-12 10:19:20 -0500

    migracion

--- bgproc.sh
+++ bgproc.sh
@@ -5,10 +5,24 @@ while (true)
 do
   i=$(($i+1))
   echo 'Procesando tomo #' $i >> /tmp/log_messages
-  psql -w -U postgres -d planillas -c "copy (select * from get_planilla_tomo($i)) to STDOUT WITH CSV DELIMITER '|' QUOTE '\"';" >> /tmp/data.csv
-  sleep 1
-  if [ $i -gt 100 ]
+  psql -w -U postgres -d planillas -c "copy (select * from get_planilla_tomo(6, $i)) to STDOUT WITH CSV DELIMITER '|' QUOTE '\"';" >> /tmp/planillas.csv
+  if [ $i -gt 420 ]
   then
      break
   fi
 done
+i=$(($i+1))
+echo 'Procesando conceptos' >> /tmp/log_messages
+psql -w -U postgres -d planillas -c "COPY (SELECT codi_conc_tco, codi_oper_ope, codi_cicl_cic, desc_conc_tco, desc_cort_tco, tipo_conc_tco, tipo_calc_tco, secu_calc_tco, flag_asoc_tco, flag_recu_tco, rnta_qnta_tco, cts_cts_tco, codi_conc_onc, codi_enti_ent, cnta_debe_tco, cnta_habe_tco, clas_conc_tco, flag_pago_tco, sede_conc_tco  FROM conceptos) TO STDOUT WITH CSV DELIMITER '|' QUOTE '\"'" >> /tmp/conceptos.csv
+i=$(($i+1))
+echo 'Procesando folios' >> /tmp/log_messages
+psql -w -U postgres -d planillas -c "COPY (SELECT codi_folio, num_folio, per_folio, reg_folio, subt_plan_stp, codi_tomo, tipo_plan_tpl, tipo_folio FROM folios) to STDOUT WITH CSV DELIMITER '|' QUOTE '\"';" >> /tmp/folios.csv
+i=$(($i+1))
+echo 'Procesando tomos' >> /tmp/log_messages
+psql -w -U postgres -d planillas -c "COPY (SELECT codi_tomo, per_tomo, ano_tomo, folios_tomo, desc_tomo  FROM tomos) to STDOUT WITH CSV DELIMITER '|' QUOTE '\"';" >> /tmp/tomos.csv
+i=$(($i+1))
+echo 'Procesando tipos de planillas' >> /tmp/log_messages
+psql -w -U postgres -d planillas -c "COPY (SELECT tipo_plan_tpl, desc_tipo_tpl, tarj_inic_tpl, tarj_fina_tpl, cant_peri_tpl, codi_oper_ope, abrev_tipo_tpl FROM tplanilla) to STDOUT WITH CSV DELIMITER '|' QUOTE '\"';" >> /tmp/tplanilla.csv
+i=$(($i+1))
+echo 'Procesando tipos conceptos por folio' >> /tmp/log_messages
+psql -w -U postgres -d planillas -c "COPY (SELECT id, orden_conc_folio, codi_folio, codi_conc_tco  FROM conceptos_folios) to STDOUT WITH CSV DELIMITER '|' QUOTE '\"';" >> /tmp/conceptosfolios.csv
